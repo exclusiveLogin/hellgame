b9f45bb213eedc6be9fc5b42c944338b196adb68
exclusivelogin <alter4444@gmail.com>
2016-05-06 15:25:27 +0400

    emocore ver 0.9 beta

--- emocore.php
+++ emocore.php
@@ -1,21 +1,55 @@
 <?php
 include_once "dbsetting.php";
-$mysql= new mysqli($dbhost,$logindb,$passdb,$dbname);
-if($mysql->connect_errno)die("error db:".$mysql->connect_error);
 
-$_GET[''];
 
-$query="SELECT  `value` FROM  `msn_emo` WHERE id = (SELECT MAX( `id`) FROM `msn_emo` )";
+$target_user = $_GET['t_user'];
+if ($target_user){
+    $mysql= new mysqli($dbhost,$logindb,$passdb,$dbname);
+    if($mysql->connect_errno){
+        die('{"errors":true,"errormsg":"error db":"'.$mysql->connect_error.'"}');
+    }
 
-$res = $mysql->query($query);
-$row = $res->fetch_assoc();
-echo '{"r_users":[';
-while($row){
-    $jsonout = json_encode($row);
-    echo $jsonout;
+    //проверка юзера есть ли в базе такой
+    $query='SELECT  `id` FROM  `users` WHERE `login` = "'.$target_user.'"';
+    $res = $mysql->query($query);
     $row = $res->fetch_assoc();
-    if($row)echo ",";
+    if(!$row["id"])die('{"errors":true,"errormsg":"user from query not exists from DB"}');
+    $res->free();
+    $row = null;
+    //----------------------------------------
+    //-----определяем последнее настроение----
+    $query="SELECT  `value` FROM  `".$target_user."_emo` WHERE id = (SELECT MAX( `id`) FROM `".$target_user."_emo` )";
+    $res = $mysql->query($query);
+    $row = $res->fetch_assoc();
+
+    if($row){
+        $last_emo = $row['value'];
+    }
+    else{
+        die('{"errors":true,"errormsg":"data lastEMO from DB is null"}');
+    }
+    $res->free();
+    $row = null;
+    //-----определяем предпоследнее настроение----
+
+    $query="SELECT  `value` FROM  `".$target_user."_emo` WHERE id = (SELECT MAX( `id`)-1 FROM `".$target_user."_emo` )";
+    $res = $mysql->query($query);
+
+    $row = $res->fetch_assoc();
+    if($row){
+        $prev_emo = $row['value'];
+    }
+    else{
+        die('{"errors":true,"errormsg":"data prevEMO from DB is null"}');
+    }
+    $res->free();
+    $mysql->close();
+
+    echo '{"last_emo":'.$last_emo.',"prev_emo":'.$prev_emo.'}';
 }
-echo "]}";
-$res->close();
-$mysql->close();
+else{
+    die('{"errors":true,"errormsg":"target user is null"}');
+}
+
+
+
